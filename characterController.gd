22d8df24bae5b6a7d0e650a430833e4baebe19ca
The-Coding-Gay <finleywhittington@gmail.com>
2024-09-20 21:06:18 +0200

    Tidy up
    Tidied up redundant files and renamed charcontroller for clarity

--- characterController.gd
+++ characterController.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
 
-const GRAVITY = 200.0
+
+const SPEED = 700.0
+const JUMP_VELOCITY = -700.0
 
 
-func _ready() -> void:
-	pass
-	
-func _physics_process(delta):
-	velocity.y += delta * GRAVITY
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-	var motion = velocity * delta
-	move_and_collide(motion)
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
