f34bd82926c2ea707313ad5c7b5ba44c22b60326
The-Coding-Gay <finleywhittington@gmail.com>
2024-09-20 22:02:02 +0200

    Tweaked the animations
    Tweaked the animations, and made the game go into fullscreen in code

--- characterController.gd
+++ characterController.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 700.0
 const JUMP_VELOCITY = -700.0
-
 @onready var _animated_sprite = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -22,8 +22,6 @@ func _physics_process(delta: float) -> void:
 		
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		_animated_sprite.play("jump")
-		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
---
+++ fullscreen.gd
@@ -0,0 +1,3 @@
+extends Node2D
+func _ready() -> void:
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
