cf46578abb74ebc49c75591beb9eb8853685599e
The-Coding-Gay <finleywhittington@gmail.com>
2024-09-20 21:49:03 +0200

    animated alittle
    added some animations. shitty code - not perfect but works a bit

--- characterController.gd
+++ characterController.gd
@@ -4,15 +4,26 @@ extends CharacterBody2D
 const SPEED = 700.0
 const JUMP_VELOCITY = -700.0
 
+@onready var _animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
+		
+	
+	if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right") and is_on_floor():
+		_animated_sprite.play("walk")
+	elif not is_on_floor():
+		_animated_sprite.play("jump")
+	else:
+		_animated_sprite.play("idle")
+	
+		
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		_animated_sprite.play("jump")
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
