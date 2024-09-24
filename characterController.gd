182975131d5f24c806ca69499f05f8bdd392f00a
The-Coding-Gay <finleywhittington@gmail.com>
2024-09-24 17:15:17 +0200

    Cleaned and added some things
    - Added code comments for clarity
    - Removed unused variables
    - Added flipping to character animation
    - Cleaned up if tree structure
    - Replaced default ui actions with custom ones
    -Changed the jump animation name for clarity

--- characterController.gd
+++ characterController.gd
@@ -1,37 +1,32 @@
 extends CharacterBody2D
 
+#get the animated sprite node when loaded
+@onready var _animated_sprite = $AnimatedSprite2D
 
 const SPEED = 700.0
 const JUMP_VELOCITY = -900.0
-const mult = 60
-const GRAVITY = 9.8
-var air_time = 0.0
-const BONUS_GRAVITY = 2.0
-@onready var _animated_sprite = $AnimatedSprite2D
-
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta * (2 if velocity.y > 0 else 1)
-		_animated_sprite.play("jump")
 	
-	if is_on_floor():
-		if Input.is_action_pressed("ui_left") or Input.is_action_pressed("ui_right"):
-			_animated_sprite.play("walk")
-		
-		else:
-			_animated_sprite.play("idle")
-		
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	#get value between -1 and 1 according to input direction
+	var movement := Input.get_axis("move_left", "move_right")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	#Handle acceleration, animation, and flipping
+	if movement: 
+		velocity.x = movement * SPEED
+		_animated_sprite.play("walk")
+		_animated_sprite.flip_h = true if velocity.x < 0 else false
+	#handle deceleration and idle animation
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		_animated_sprite.play("idle")
+		
+	#apply gravity and play jump animation
+	if not is_on_floor():
+		velocity += get_gravity() * delta * (2 if velocity.y > 0 else 1)
+		_animated_sprite.play("in the air")
+	
+	#make the character jump
+	elif Input.is_action_just_pressed("jump"): velocity.y = JUMP_VELOCITY
+		
 	move_and_slide()
