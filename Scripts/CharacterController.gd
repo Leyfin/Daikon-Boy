20bda53a48707cc45eae100eb9ae3bce7147887b
The-Coding-Gay <finleywhittington@gmail.com>
2024-09-26 00:20:37 +0200

    Added primitive bury
    changed the spritesheet, and added a primitive version of the bury mechanic, no hitbox changes just sprite animation.
    
    -- code needs to be tidied

--- Scripts/CharacterController.gd
+++ Scripts/CharacterController.gd
@@ -5,28 +5,37 @@ extends CharacterBody2D
 
 @export var SPEED: float = 550.0
 @export var JUMP_VELOCITY: float = -700.0
+var isBuried: bool = false
 
 func _physics_process(delta: float) -> void:
-	
 	##get value between -1 and 1 according to input direction
 	var movement: float = Input.get_axis("move_left", "move_right")
+
+	if Input.is_action_just_pressed("bury"):
+		isBuried = true
 
 	#Handle acceleration, animation, and flipping
 	if movement: 
 		velocity.x = movement * SPEED
-		_animated_sprite.play("walk")
+		if isBuried == false: _animated_sprite.play("walk")
+		else: _animated_sprite.play("buried")
 		_animated_sprite.flip_h = true if velocity.x < 0 else false
+	
 	#handle deceleration and idle animation
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		_animated_sprite.play("idle")
-		
-	#apply gravity and play jump animation
+		if isBuried == false: _animated_sprite.play("idle")
+		elif movement == 0:
+			_animated_sprite.play("peek")
+
+	#apply gravity and play in the air animation
 	if not is_on_floor():
 		velocity += get_gravity() * delta * (3 if velocity.y > 0 else 2)
 		_animated_sprite.play("in the air")
 	
 	#make the character jump
-	elif Input.is_action_just_pressed("jump"): velocity.y = JUMP_VELOCITY
+	elif Input.is_action_just_pressed("jump"): 
+		velocity.y = JUMP_VELOCITY
+		isBuried = false
 		
 	move_and_slide()
