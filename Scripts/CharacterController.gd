7a496ca80ddb5f9df6e8e1cfe955b1bd1fc0f524
The-Coding-Gay <finleywhittington@gmail.com>
2024-09-30 01:18:23 +0200

    Changed variable names for clarity
    Made some variable and function names more clear, and made some variables more statically defined

--- Scripts/CharacterController.gd
+++ Scripts/CharacterController.gd
@@ -1,43 +1,42 @@
 extends CharacterBody2D
 
-#get the animated sprite node when loaded
 @onready var _animated_sprite = $AnimatedSprite2D
-@onready var defCollider = $PlayerCollider
+@onready var defaultCollider = $PlayerCollider
 @onready var buriedCollider = $BuriedCollider
 @export var SPEED: float = 550.0
 @export var JUMP_VELOCITY: float = -700.0
-@export var isBuried: bool
+@export var CurrentBuriedState: bool
 
-func Anim(animationName, shouldPlayWhenBuried):
-	if shouldPlayWhenBuried == isBuried: _animated_sprite.play(animationName)
+func Animate(animationName: String, shouldPlayWhenBuried: bool):
+	if shouldPlayWhenBuried == CurrentBuriedState: _animated_sprite.play(animationName)
 
 func _physics_process(delta: float) -> void:
 	#get value between -1 and 1 according to input direction
 	var movement: float = Input.get_axis("move_left", "move_right")
 	
 	if Input.is_action_just_pressed("bury"):
-		isBuried = true
-		defCollider.disabled = true
+		CurrentBuriedState = true
+		defaultCollider.disabled = true
 		buriedCollider.disabled = false
 	#Handle acceleration, animation, and flipping
 	if movement: 
 		velocity.x = movement * SPEED
-		Anim("walk", false)
-		Anim("buried", true)
+		Animate("walk", false)
+		Animate("buried", true)
 		_animated_sprite.flip_h = true if velocity.x < 0 else false
 	#handle deceleration and idle animation
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		Anim("idle", false)
-		Anim("peek", true)
+		Animate("idle", false)
+		Animate("peek", true)
 	#apply gravity and play in the air animation
 	if not is_on_floor():
 		velocity += get_gravity() * delta * (3 if velocity.y > 0 else 2)
-		Anim("in the air", false)
+		Animate("in the air", false)
 	#make the character jump
 	elif Input.is_action_just_pressed("jump"): 
 		velocity.y = JUMP_VELOCITY
-		isBuried = false
-		defCollider.disabled = false
+		CurrentBuriedState = false
+		defaultCollider.disabled = false
 		buriedCollider.disabled = true
 	move_and_slide()
